0d433c4b8251ff0ad4af7489885e5be152298ef4
Iccprsm <76848222+beer55@users.noreply.github.com>
2022-07-21 23:54:15 +0200

    Act

---
+++ tiempo.py
@@ -0,0 +1,14 @@
+def tierra(t):
+	g = 9.8
+	tiempo =1/2*g*t^2
+	return tiempo
+	
+def marte(t):
+	g=3.7
+	tiempo = 1/2*g*t^2
+	return tiempo
+
+def jupyter(t):
+	g=24.9
+	tiempo = 1/2*g*t^2
+	return tiempo
